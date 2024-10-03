832581d111e505d88102b7e0110bf4eea0bc3ad2
hz2870 <hz2870@columbia.edu>
2024-10-03 12:43:02 -0400

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,16 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    bondPrice = 0
+    coupon = face * couponRate
+    
+    for a, b in zip(yc, times):
+        if b != times[-1]:
+            cashflow = coupon
+        else:
+            cashflow = coupon + face
+        dcf = 1/(1+a)**b
+        bondPrice += dcf * cashflow
+                
+        
+    return(bondPrice)
